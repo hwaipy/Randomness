6661c50362c0da1f9920fe59c1f46314c6ff196a
Hwaipy Li <hwaipy@gmail.com>
2014-03-12 02:06:17 +0800

    Doc added
    Doc added for Device and DeviceException

--- 2013-09-08 LightSecondDeviceControl/UnifiedDeviceInterface/src/com/hwaipy/unifieddeviceinterface/Device.java
+++ 2013-09-08 LightSecondDeviceControl/UnifiedDeviceInterface/src/com/hwaipy/unifieddeviceinterface/Device.java
@@ -1,12 +1,23 @@
 package com.hwaipy.unifieddeviceinterface;
 
 /**
+ * An abstract representation of device. fewafwae
  *
  * @author HwaipyLab
  */
 public interface Device {
 
+    /**
+     * Open this device.
+     *
+     * @throws DeviceException
+     */
     public void open() throws DeviceException;
 
+    /**
+     * Close this device.
+     *
+     * @throws DeviceException
+     */
     public void close() throws DeviceException;
 }
