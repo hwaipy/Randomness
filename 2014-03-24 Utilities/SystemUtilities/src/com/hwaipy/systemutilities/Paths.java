2b6bcfd497a595efb6771d935134e94fa96ddd29
Hwaipy Li <hwaipy@gmail.com>
2014-04-01 16:29:13 +0800

    add Doc

--- 2014-03-24 Utilities/SystemUtilities/src/com/hwaipy/systemutilities/Paths.java
+++ 2014-03-24 Utilities/SystemUtilities/src/com/hwaipy/systemutilities/Paths.java
@@ -3,7 +3,9 @@ package com.hwaipy.systemutilities;
 import java.nio.file.Path;
 
 /**
+ * 提供系统默认路径。数据从{@link com.hwaipy.systemutilities.Properties Properties}获取。
  *
+ * @see com.hwaipy.systemutilities.Properties
  * @author Hwaipy
  */
 public class Paths {
@@ -11,6 +13,10 @@ public class Paths {
     private static final String KEY_PATH_DATA_STORAGY = "KeyPathDataStorage";
     private static final String DEF_PATH_DATA_STORAGY = ".";
 
+    /**
+     *
+     * @return
+     */
     public static Path getDataStoragyPath() {
         String directory = Properties.getProperty(KEY_PATH_DATA_STORAGY, DEF_PATH_DATA_STORAGY);
         Path path = java.nio.file.Paths.get(directory);
