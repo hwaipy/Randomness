a740a92bbdab5429f9275eeae04b8584fef8d02f
Hwaipy Li <hwaipy@gmail.com>
2014-10-16 14:03:16 +0800

    g2

---
+++ 2013-09-08 LightSecondDeviceControl/LightSecondDataAnalyzerApplication/src/lightseconddataanalyzer/g2/CoincidenceScaner.java
@@ -0,0 +1,71 @@
+package lightseconddataanalyzer.g2;
+
+import com.hwaipy.unifieddeviceinterface.timeeventdevice.timeeventcontainer.TimeEventList;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author Administrator
+ */
+class CoincidenceScaner {
+
+    private static final int SCAN_RANGE = 1000000;
+    private final TimeEventList list1;
+    private final TimeEventList list2;
+
+    CoincidenceScaner(TimeEventList list1, TimeEventList list2) {
+        this.list1 = list1;
+        this.list2 = list2;
+    }
+
+    public List<Integer> scanOfPeaks() {
+        int[] stat = scan();
+
+        //period=125,start at 34,gate=30 for range of 100000ps
+        //period=125,start at 35,gate=30 for range of 1000000ps
+        ArrayList<Integer> peaks = new ArrayList<>();
+        for (int iCenter = 35; iCenter < 20000; iCenter += 125) {
+            int iStart = iCenter - 14;
+            int iEnd = iCenter + 14;
+            int sum = 0;
+            for (int i = iStart; i <= iEnd; i++) {
+                sum += stat[i];
+            }
+            peaks.add(sum);
+            System.out.println(iCenter + "\t" + sum);
+        }
+        return peaks;
+    }
+
+    public int[] scan() {
+        int stepLength = 100;
+        int channelCount = SCAN_RANGE * 2 / stepLength + 1;
+        int[] stat = new int[channelCount];
+        int indexStart2 = 0;
+        for (int index1 = 0; index1 < list1.size(); index1++) {
+            long time1 = list1.get(index1).getTime();
+            long time2Start = time1 - SCAN_RANGE;
+            long time2End = time1 + SCAN_RANGE;
+            while (indexStart2 < list2.size()) {
+                long time2 = list2.get(indexStart2).getTime();
+                if (time2 > time2Start) {
+                    break;
+                }
+                indexStart2++;
+            }
+            int index2 = indexStart2;
+            while (index2 < list2.size()) {
+                long time2 = list2.get(index2).getTime();
+                if (time2 > time2End) {
+                    break;
+                }
+                int diffTime = (int) (time2 - time1 + SCAN_RANGE);
+                int diffChannel = diffTime / stepLength;
+                stat[diffChannel]++;
+                index2++;
+            }
+        }
+        return stat;
+    }
+}
---
+++ 2013-09-08 LightSecondDeviceControl/LightSecondDataAnalyzerApplication/src/lightseconddataanalyzer/g2/G2.java
@@ -0,0 +1,35 @@
+package lightseconddataanalyzer.g2;
+
+import com.hwaipy.unifieddeviceinterface.DeviceException;
+import com.hwaipy.unifieddeviceinterface.timeeventdevice.data.TimeEventDataManager;
+import com.hwaipy.unifieddeviceinterface.timeeventdevice.hydraharp400data.HydraHarp400T2Loader;
+import com.hwaipy.unifieddeviceinterface.timeeventdevice.timeeventcontainer.TimeEventList;
+import com.hwaipy.unifieddeviceinterface.timeeventdevice.timeeventcontainer.TimeEventSegment;
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+/**
+ *
+ * @author Hwaipy
+ */
+public class G2 {
+
+    public static void main(String[] args) throws IOException, DeviceException {
+        File file = new File("/Users/Hwaipy/Desktop/G2/201410161205.ht2");
+        HydraHarp400T2Loader loader = new HydraHarp400T2Loader(file);
+        TimeEventSegment segment = TimeEventDataManager.loadTimeEventSegment(loader);
+
+        TimeEventList signalList1 = segment.getEventList(1);
+        TimeEventList signalList2 = segment.getEventList(8);
+        System.out.println("Signal 1: " + signalList1.size());
+        System.out.println("Signal 2: " + signalList2.size());
+
+        CoincidenceScaner scaner = new CoincidenceScaner(signalList1, signalList2);
+        List<Integer> stat = scaner.scanOfPeaks();
+//        int[] stat = scaner.scan();
+        for (int t : stat) {
+            System.out.println(t);
+        }
+    }
+}
